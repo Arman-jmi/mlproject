3938bc23b8baff12b31e888ff6470a2a26704f7b
Arman-jmi <syedarman7863@gmail.com>
2026-09-22 16:26:50 +0530

    EDA

--- src/exception.py
+++ src/exception.py
@@ -1,4 +1,6 @@
 import sys
+from src.logger import logging
+
 
 def error_message_detail(error , error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
@@ -13,6 +15,3 @@ class CustomException(Exception):
     def __init__(self, error_message , error_detail:sys):
         super().__init__(error_message)
         self.error_message=error_message_detail(error_message, error_detail=error_detail)
-
-    def __Str__(self):
-        return self.error_message
